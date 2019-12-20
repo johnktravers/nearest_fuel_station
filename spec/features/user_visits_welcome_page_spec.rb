6fe8966535fc53b8d0a199e4e1e6f65030b8c9eb
John Travers <johnk.travers@gmail.com>
2019-12-20 11:07:19 -0700

    Use VCR for nearest station feature tests and nrel service test

--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -10,21 +10,23 @@ describe 'user can visit the welcome page' do
   end
 
   it 'can find the nearest electric charging station to a location' do
-    visit root_path
+    VCR.use_cassette('station_search_results') do
+      visit root_path
 
-    select 'Turing', from: :location
-    click_button 'Find Nearest Station'
+      select 'Turing', from: :location
+      click_button 'Find Nearest Station'
 
-    expect(current_path).to eq(search_path)
+      expect(current_path).to eq(search_path)
 
-    within '.search-results' do
-      expect(first('#station-name').text).to_not be_empty
-      expect(page).to have_css('#address')
-      expect(page).to have_css('#fuel-type')
-      expect(page).to have_css('#access-times')
-      expect(page).to have_css('#distance')
-      expect(page).to have_css('#travel-time')
-      expect(page).to have_css('#directions')
+      within '.search-results' do
+        expect(first('#station-name').text).to_not be_empty
+        expect(page).to have_css('#address')
+        expect(page).to have_css('#fuel-type')
+        expect(page).to have_css('#access-times')
+        expect(page).to have_css('#distance')
+        expect(page).to have_css('#travel-time')
+        expect(page).to have_css('#directions')
+      end
     end
   end
 end
