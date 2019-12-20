2d0c2bb5d6881bdd309603c1b4fe0555f22cb4c5
John Travers <johnk.travers@gmail.com>
2019-12-20 10:07:10 -0700

    Create test that user can find the nearest electric charging station

--- spec/features/user_visits_welcome_page_spec.rb
+++ spec/features/user_visits_welcome_page_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
-describe "user can visit the welcome page" do
-  scenario "and see a dropdown menu" do
-    visit '/'
-    expect(page).to have_content("Nearest Fuel Station")
-    expect(page).to have_content("Search For The Nearest Electric Charging Station")
-    expect(page).to have_button("Find Nearest Station")
+describe 'user can visit the welcome page' do
+  it 'and see a dropdown menu' do
+    visit root_path
+
+    expect(page).to have_content('Nearest Fuel Station')
+    expect(page).to have_content('Search For The Nearest Electric Charging Station')
+    expect(page).to have_button('Find Nearest Station')
+  end
+
+  it 'can find the nearest electric charging station to a location' do
+    visit root_path
+
+    select 'Turing', from: :location
+    click_button 'Find Nearest Station'
+
+    expect(current_path).to eq(search_path)
+
+    within '.search-results' do
+      expect(first('#station-name').text).to_not be_empty
+      expect(page).to have_css('#address')
+      expect(page).to have_css('#fuel-type')
+      expect(page).to have_css('#access-times')
+    end
   end
 end
